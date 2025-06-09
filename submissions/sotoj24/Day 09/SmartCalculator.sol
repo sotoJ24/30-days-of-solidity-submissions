1190baf6c5e886c9a32a4b72ad36eb69725683d9
Josue Soto <sotoherrerajosue@gmail.com>
2025-06-09 17:51:50 -0600

    Feat: Smart Calculator Day 09

---
+++ submissions/sotoj24/Day 09/SmartCalculator.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Calculator {
+    function add(uint256 a, uint256 b) external pure returns (uint256) {
+        return a + b;
+    }
+
+    function subtract(uint256 a, uint256 b) external pure returns (uint256) {
+        return a - b;
+    }
+
+    function multiply(uint256 a, uint256 b) external pure returns (uint256) {
+        return a * b;
+    }
+
+    function divide(uint256 a, uint256 b) external pure returns (uint256) {
+        require(b != 0, "Cannot divide by zero");
+        return a / b;
+    }
+}
